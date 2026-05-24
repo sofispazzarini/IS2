ff42b93a40bb4ce1a055fb559ed7fa09d93e75da
Ines Montalvo <montalvoines@gmail.com>
2026-05-24 12:40:24 -0300

    viendo mp

--- pago/views.py
+++ pago/views.py
@@ -10,6 +10,25 @@ from turno.models import Reserva
 from .models import Pago
 
 sdk = mercadopago.SDK(settings.MERCADO_PAGO_ACCESS_TOKEN)
+print(settings.MERCADO_PAGO_ACCESS_TOKEN)
+
+#HOLA
+def crear_pago(request):
+    preference_data = {
+        "items": [
+            {
+                "title": "Test",
+                "quantity": 1,
+                "unit_price": 100
+            }
+        ]
+    }
+
+    preference_response = sdk.preference().create(preference_data)
+    print(preference_response)
+
+    return redirect(preference_response["response"]["init_point"])
+#CHAU
 
 @login_required
 def pagar_con_mercadopago(request, reserva_id):
