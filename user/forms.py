04bc3616edb33c6109bce26b7aacbe967239f55c
Ines Montalvo <montalvoines@gmail.com>
2026-06-26 19:40:23 -0300

    Cambio mensajes al crear profesor

--- user/forms.py
+++ user/forms.py
@@ -50,7 +50,7 @@ class ProfesorForm(forms.ModelForm):
             
         # 4. Si el queryset todavía tiene algún resultado, tiramos el error
         if queryset.exists():
-            raise forms.ValidationError("Ya existe un profesor asociado al correo electrónico ingresado.")
+            raise forms.ValidationError("Error: Ya existe un profesor asociado al correo electrónico ingresado.")
             
         return email
 
@@ -66,7 +66,7 @@ class ProfesorForm(forms.ModelForm):
             queryset = queryset.exclude(pk=self.instance.pk)
 
         if queryset.exists():
-            raise forms.ValidationError(f"Ya existe un profesor asociado al DNI {dni}.")
+            raise forms.ValidationError(f"Error: Ya existe un profesor asociado al DNI {dni}.")
 
         return dni
 
