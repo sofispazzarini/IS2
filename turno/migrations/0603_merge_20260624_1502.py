c8e22bfe9a5084d1926a39aedf3e5ae6a2d29cb2
sele <selenemoscardi5@gmail.com>
2026-06-28 16:18:38 -0300

    subo cambio de mantenimiento

--- turno/migrations/0603_merge_20260624_1502.py
+++ turno/migrations/0603_merge_20260624_1502.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('turno', '0006_alter_clase_salon'),
+        ('turno', '0007_alter_clase_salon_fk'),
         ('turno', '0602_remove_abono_turnos_fijos_ids'),
     ]
 
