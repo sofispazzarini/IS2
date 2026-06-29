d722c21be2dee901044aa202f08133cc0434b7d8
Ines Montalvo <montalvoines@gmail.com>
2026-06-29 19:43:28 -0300

    conf

--- turno/views.py
+++ turno/views.py
@@ -1813,7 +1813,6 @@ def registrar_pago_abono_admin(request, user_id):
         'tiene_reservas': bool(reservas_disponibles),
     })
 
-<<<<<<< HEAD
 def actualizar_ausentes_y_penalizar():
     """
     Busca clases finalizadas hace más de 30 minutos y pasa las reservas 
@@ -1873,7 +1872,6 @@ def actualizar_ausentes_y_penalizar():
                     print(f"El usuario {usuario.username} ya tiene {penalizaciones_actuales} penalizaciones. No se agrega otra.")
 
     return len(reservas_a_ausentes)
-=======
 @login_required
 def historial_clases(request):
     """Historial de clases pasadas para el dueño."""
@@ -1889,4 +1887,3 @@ def historial_clases(request):
     return render(request, 'turno/historial_clases.html', {
         'clases': clases,
     })
->>>>>>> origin/feature/sele
